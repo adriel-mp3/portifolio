98330685eb95ccb042903dcec1db921f5ad5f35a
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-06-02 15:04:06 -0300

    :sparkles: feat(js): add to slide arrow navigation #11

--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@ import { SlideNav } from './modules/slide.js';
 
 const slide = new SlideNav('.slide','.slide-wrapper');
 slide.init();
-// slide.addArrow('.prev', '.next');
+slide.addArrow('.prev', '.next');
